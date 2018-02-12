36d8695670721e505ed9b3dfe4c98e932a65ecf5
stephen <stephenrs7@gmail.com>
2018-02-12 13:19:09 -0800

    added homepage components

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = props => (
+    <header>
+        <h5>Weclcome to</h5>
+        <h1>date.create</h1>
+        <h4>use date.create SELECT partner WHERE compatiblity = TRUE</h4>
+        
+    </header>
+);
+
+export default Header;
---
+++ client/src/components/createUser/createUser.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./createUser.css";
+
+const createUser = (props) => (
+    <div 
+        className="col-md-4" 
+        // style={{backgroundImage: `url("${props.image}")`}}
+        // onClick= {() => props.updateScore(props)}
+    >
+    </div>
+);
+
+export default createUser;
