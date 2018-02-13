a7bba972782f69bb12f1b011b2c59821e80e52e7
Levi Crouch <levi.crouch@gmail.com>
2018-02-12 22:18:17 -0800

    Adding routes for inteacting with the DB

--- routes/api/DateCreateUser.js
+++ routes/api/DateCreateUser.js
@@ -7,15 +7,11 @@ router.route("/")
   .post(DateCreateUserController.createUser);
 
 // Matches with "/api/DateCreateUsers/:id"
-router
-  .route("/:id")
+router.route("/:id")
   .get(DateCreateUserController.findById)
   .put(DateCreateUserController.updateRecord);
 
-// Matches with "/api/DateCreateUsers/:id/search/:locCity/:locState/:gender"
-router
-  // .route("/:id/search/city=:locCity?state=:locState/gender=:gender")
-  .route("/:id/search/city\=:locCity&state\=:locState&gender\=:gender")
-  // 5a826338fe24662c0c4054e3
+// Matches with api/DateCreateUsers/:id/search/city=:locCity&state=:locState&gender=:gender
+router.route("/:id/search/city\=:locCity&state\=:locState&gender\=:gender")
   .get(DateCreateUserController.findMatch)
 module.exports = router;
