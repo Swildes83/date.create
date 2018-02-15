db1318874e520e50c893753349862e4990f5974d
Spencer Wildes <spencerwildes@gmail.com>
2018-02-15 10:35:01 -0800

    02/15

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Header from './components/Header';
 import CreateUser from './components/CreateUser';
 import ContactQuiz from './components/contactQuiz';
 import ProfileCard from './components/ProfileCard/ProfileCard';
-import PublicProfile from './components/UserPages/publicProfile';
 import './App.css';
 import { Button, Card, Row, Col } from 'react-materialize';
 import PublicProfile from "./components/UserPages/publicProfile";
@@ -18,14 +17,10 @@ class App extends Component {
           
           <Switch>
         
-            <Route exact path="/:id/public" component={PublicProfile}/>
+        
             <Route exact path="/:id/public/search" component={ProfileCard}/>
             <Route exact path="/:id/public/quiz" component={ContactQuiz} />
-<<<<<<< HEAD
-            
-=======
             <Route exact path="/:id/profile" component={PublicProfile} />
->>>>>>> 45063128950d03cbc30024dd5ecff50f2932609d
           </Switch>
         </div>
       </Router>
