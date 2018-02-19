07785d013b44425309fe92614ad3a2527affa78e
Levi Crouch <levi.crouch@gmail.com>
2018-02-18 21:28:27 -0800

    Added a requirements form

--- client/src/components/UserPages/PrivateProfile/PrivateProfile.js
+++ client/src/components/UserPages/PrivateProfile/PrivateProfile.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import {Route, NavLink, HashRouter} from "react-router-dom";
+import { Route, NavLink, HashRouter } from "react-router-dom";
 import "./PrivateProfile.css";
 import API from "../../../utils/API";
 import Placeholder from "../Placeholder";
+import Requirements from "./Requirements";
 
 const userID = "5a8624f2fe9f971c5c565bb3";
 
@@ -21,34 +22,42 @@ class PrivateProfile extends Component {
     };
     render() {
         return (
-            <div className="card">
-                <div className="card-content">
-                    <img id="profileimage" src="../assets/profile/ChickenKiller/profile.png" alt="My profile"></img>
-                    <p id="name">{this.state.result.screenName}</p>
+            <HashRouter>
+                <div className="card">
+                    <div className="card-content">
+                        <img id="profileimage" src="../assets/profile/ChickenKiller/profile.png" alt="My profile"></img>
+                        <p id="name">{this.state.result.screenName}</p>
+                    </div>
+                    <div className="card-tabs">
+                        <ul className="tabs tabs-fixed-width">
+                            <li className="tab">
+                                <a href="/:id/preferences">date(preferences)</a>
+                            </li>
+                            <li className="tab">
+                                <NavLink to="/requirements">date(requirements)</NavLink>
+                            </li>
+                            <li className="tab">
+                                <a href="/results">date(results)</a>
+                            </li>
+                            <li className="tab">
+                                <a href="/potentials">date(potentials)</a>
+                            </li>
+                            <li className="tab">
+                                <a href="/contacts">date(contacts)</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="private-content">
+                        <Route path="/" component={Placeholder} />
+                        <Route path="/:id/preferences" component={Placeholder} />
+                        <Route path="/requirements" component={Requirements} />
+                        <Route path="/results" component={Placeholder} />
+                        <Route path="/potentials" component={Placeholder} />
+                        <Route path="/contacts" component={Placeholder} />
+                    </div>
                 </div>
-                <div className="card-tabs">
-                    <ul className="tabs tabs-fixed-width">
-                    <li className="tab">
-                            <a href="/preferences">date(preferences)</a>
-                        </li>
-                        <li className="tab">
-                            <a href="/requirements">date(requirements)</a>
-                        </li>
-                        <li className="tab">
-                            <a href="/results">date(results)</a>
-                        </li>
-                        <li className="tab">
-                            <a href="/potentials">date(potentials)</a>
-                        </li>
-                        <li className="tab">
-                            <a href="/contacts">date(contacts)</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+            </HashRouter>
         )
-
-
     }
 }
 export default PrivateProfile;
--- client/src/components/UserPages/PrivateProfile/Requirements.js
+++ client/src/components/UserPages/PrivateProfile/Requirements.js
@@ -1,60 +1,144 @@
 import React, { Component } from "react";
-// import "./BioData.css";
-import API from "../../utils/API";
+import "./Requirements.css";
+// import API from "../../../utils/API";
+import { Row, Input, Button, Icon } from "react-materialize";
 
-// hardcoding the user ID of Peter Griffin from mongo DB
-// You will need to update this ID with the correct ID from your local mongoDb instance
-
-const userID = "5a8624f2fe9f971c5c565bb3";
-
-class BioData extends Component {
+class Requirements extends Component {
     state = {
         result: [],
         search: ""
     };
-    componentDidMount() {
-        this.getBioData(userID);
-    };
-    getBioData = (id) => {
-        API.getUserData(id)
-            .then(res =>
-                this.setState({ result: res.data }),
-        )
-            .catch(err => console.log(err));
-    };
     render() {
         return (
-            <div className="bioList">
-                <ul><b>Mantra:</b>
-                    <li>{this.state.result.bioMantra}</li>
-                </ul>
-                <ul><b>Technology:</b>
-                    <li>{this.state.result.bioTechnology}</li>
-                </ul>
-                <ul><b>AFK Hobbies:</b>
-                    <li>{this.state.result.bioAFKHobbies}</li>
-                </ul>
-                <ul><b>Movies:</b>
-                    <li>{this.state.result.bioMovies}</li>
-                </ul>
-                <ul><b>TV:</b>
-                    <li>{this.state.result.bioTV}</li>
-                </ul>
-                <ul><b>Music:</b>
-                    <li>{this.state.result.bioMusic}</li>
-                </ul>
-                <ul><b>Currently Reading:</b>
-                    <li>{this.state.result.bioCurrentlyReading}</li>
-                </ul>
-                <ul><b>Currently Watching:</b>
-                    <li>{this.state.result.bioCurrentlyWatching}</li>
-                </ul>
-                <ul><b>Currently Listening To:</b>
-                    <li>{this.state.result.bioCurrentlyListening}</li>
-                </ul>
-            </div >)
-
-
+            <div className="requirement-form">
+                <h3>date(requirements)</h3>
+                <p>Enter your requirements for your perfect match</p>
+                <Row>
+                    <h4>date(requirements.gender)</h4>
+                    <Input
+                        type='select'
+                        s={12}
+                        label='Gender'>
+                        <option value='F'>Female</option>
+                        <option value='M'>Male</option>
+                        <option value='NS'>Not Specified</option>
+                    </Input>
+                </Row>
+                <Row>
+                <h4>date(requirements.technology)</h4>
+                <p>comma separated list</p>
+                    <Input
+                        s={12}
+                        placeholder="E.g: JavaScript,CSS,HTML5,'Web Development'"
+                        label='Technology'>
+                    </Input>
+                </Row>
+                <Row>
+                <h4>date(requirements.gaming)</h4>
+                <p>comma separated list</p>
+                    <Input
+                        s={12}
+                        placeholder="E.g: 'Destiny 2','Injustice 2',PUBG,Overwatch,'Quake 2'"
+                        label='Gaming'>
+                    </Input>
+                </Row>
+                <Row>
+                <h4>date(requirements.location(city))</h4>
+                    <Input
+                        s={12}
+                        placeholder="E.g: 'San Diego','Los Angeles',Springfield,Quahog"
+                        label='City'>
+                    </Input>
+                </Row>
+                <Row>
+                <h4>date(requirements.location(state))</h4>
+                    <Input
+                        type='select'
+                        s={12}
+                        label='State'>
+                        <option value='CAS'>Choose a state</option>
+                        <option value='AL'>Alabama</option>
+                        <option value='AK'>Alaska</option>
+                        <option value='AS'>American Samoa</option>
+                        <option value='AZ'>Arizona</option>
+                        <option value='AR'>Arkansas</option>
+                        <option value='CA'>California</option>
+                        <option value='CO'>Colorado</option>
+                        <option value='DC'>District of Columbia</option>
+                        <option value='DE'>Delaware</option>
+                        <option value='FL'>Florida</option>
+                        <option value='GA'>Georgia</option>
+                        <option value='GU'>Guam</option>
+                        <option value='HI'>Hawaii</option>
+                        <option value='ID'>Idaho</option>
+                        <option value='IL'>Illinois</option>
+                        <option value='IN'>Indiana</option>
+                        <option value='IA'>Iowa</option>
+                        <option value='KS'>Kansas</option>
+                        <option value='KY'>Kentucky</option>
+                        <option value='LA'>Louisiana</option>
+                        <option value='ME'>Maine</option>
+                        <option value='MD'>Maryland</option>
+                        <option value='MA'>Massachusetts</option>
+                        <option value='MI'>Michigan</option>
+                        <option value='MN'>Minnesota</option>
+                        <option value='MS'>Mississippi</option>
+                        <option value='MO'>Missouri</option>
+                        <option value='MT'>Montana</option>
+                        <option value='NE'>Nebraska</option>
+                        <option value='NV'>Nevada</option>
+                        <option value='NH'>New Hampshire</option>
+                        <option value='NJ'>New Jersey</option>
+                        <option value='NM'>New Mexico</option>
+                        <option value='NY'>New York</option>
+                        <option value='NC'>North Carolina</option>
+                        <option value='MP'>North Marianas Islands</option>
+                        <option value='ND'>North Dakota</option>
+                        <option value='OH'>Ohio</option>
+                        <option value='OK'>Oklahoma</option>
+                        <option value='OR'>Oregon</option>
+                        <option value='PA'>Pennsylvania</option>
+                        <option value='PR'>Puerto Rico</option>
+                        <option value='RI'>Rhode Island</option>
+                        <option value='SC'>South Carolina</option>
+                        <option value='SD'>South Dakota</option>
+                        <option value='TN'>Tennessee</option>
+                        <option value='TX'>Texas</option>
+                        <option value='UT'>Utah</option>
+                        <option value='VT'>Vermont</option>
+                        <option value='VA'>Virginia</option>
+                        <option value='VI'>Virgin Islands</option>
+                        <option value='WA'>Washington</option>
+                        <option value='WV'>West Virginia</option>
+                        <option value='WI'>Wisconsin</option>
+                        <option value='WY'>Wyoming</option>
+                    </Input>
+                </Row>
+                <Row>
+                <h4>date(requirements.age(min))</h4>
+                    <Input
+                        placeholder='Enter a minimum age:'
+                        s={6}
+                        label='Minimum Age'>
+                    </Input>
+                    <h4>date(requirements.age(max))</h4>
+                    <Input
+                        placeholder='Enter a maximum age:'
+                        s={6}
+                        label='Maximum Age'>
+                    </Input>
+                </Row>
+                <Row>
+                <h4>date(requirements.hobbies(AFK))</h4>
+                    <Input
+                        s={12}
+                        placeholder="E.g: Hiking,Camping,Skiing,ComicCon"
+                        label='Hobbies'>
+                    </Input>
+                </Row>
+                <Button className="red accent-1" waves='light'>button<Icon left>save</Icon></Button>
+            </div >
+        )
     }
 }
-export default BioData;
+export default Requirements;
