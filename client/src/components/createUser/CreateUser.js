ad7a035e842beff9e1ba29360efcf4ef48ac0aaf
stephen <stephenrs7@gmail.com>
2018-02-20 20:40:23 -0800

    nothing to commit

--- client/src/components/createUser/CreateUser.js
+++ client/src/components/createUser/CreateUser.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./CreateUser.css";
+//class CreateUser extends Component {
+    //sayName
 
+ //}
 const CreateUser = (props) => (
 
     <div className="row">
     <div className="col s6">
-    <a href="/1/profile" className="waves-effect waves-light indigo lighten-2 btn createuserbutton">public profile</a>
+        <a href="/1/profile" className="waves-effect waves-light indigo lighten-2 btn createuserbutton">create user</a>
     </div>
     <div className="col s6">
-    <a href="/1/private" className="waves-effect waves-light indigo lighten-2 btn loginbutton">private profile</a>
+        <a href="/1/private" className="waves-effect waves-light indigo lighten-2 btn loginbutton">login</a>
     </div>
     </div>
 );
